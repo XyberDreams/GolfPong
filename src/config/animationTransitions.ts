46b8795e10242136006d66e250248eb75b73fd98
TheOnlyRhy <rhymeluk@gmail.com>
2025-06-30 22:55:27 +0800

    cmon ts

--- src/config/animationTransitions.ts
+++ src/config/animationTransitions.ts
@@ -2,12 +2,17 @@ import { useState } from "react";
 import useAnimationFlow from "../hooks/useAnimationFlow";
 
 const states = ["intro", "productInfo", "finish"] as const;
-type State = typeof states[number];
+type State = (typeof states)[number];
 
 const transitionMap = {
-  "intro->productInfo": () => { /* play intro to productInfo anim */ },
-  "productInfo->intro": () => { /* play reverse anim */ },
-  "*->finish": () => { /* play finish anim */ },
+  "intro->productInfo": () => {
+    /* play intro to productInfo anim */
+  },
+  "productInfo->intro": () => {
+    /* play reverse anim */
+  },
+  "*->finish": () => {
+    /* play finish anim */
+  },
 };
 
-const { goToState } = useAnimationFlow<State>(currentState, setCurrentState, transitionMap);
