67d31b52b9168fc7b7405e4e26139b86a82ab937
TheOnlyRhy <rhymeluk@gmail.com>
2025-07-12 14:35:16 +0800

    Pre-portals stuff save file

--- src/camera/CameraSwitcher.ts
+++ src/camera/CameraSwitcher.ts
@@ -4,14 +4,24 @@ import { CurrentStateArray } from "../context/ExperienceContext";
 import { useEffect } from "react";
 import { useThree } from "@react-three/fiber";
 import * as THREE from "three";
+import mixpanel from "../mixpanel";
 
 export default function CameraSwitcher() {
   const { activeCamera, setActiveCamera, setCurrentState } = useExperience();
   const { camera } = useThree();
 
   useControls("Camera Switcher", {
-    "Blender Camera": button(() => setActiveCamera("blenderCamera")),
-    "Camera Manager": button(() => setActiveCamera("presetCamera")),
+    "Blender Camera": button(() => {
+      setActiveCamera("blenderCamera");
+      mixpanel.track("Button Clicked", { button: "Blender Camera" }); // <-- Track event
+    }),
+    "Camera Manager": button(() => {
+      setActiveCamera("presetCamera");
+      mixpanel.track("Signup Button Clicked", {
+        source: "home page",
+        variety: "default",
+      });
+    }),
     "Test Camera": button(() => setActiveCamera("animatedCamera")),
   });
 
---
+++ src/mixpanel.ts
@@ -0,0 +1,10 @@
+import mixpanel from "mixpanel-browser";
+
+mixpanel.init("86276f261dec8e4e2e6ee288e1acb820", {
+  debug: true,
+  track_pageview: true,
+  persistence: "localStorage",
+});
+
+export default mixpanel;
+
